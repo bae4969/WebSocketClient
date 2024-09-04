2b3919dc0946c570c03fb7fee32ec6b043c59705
bae4969 <bae4969@naver.com>
2024-09-04 22:52:41 +0900

    wol 서비스 실행 최초 성공

--- WebSocketClient/Classes/BaeWebSocketClient.cs
+++ WebSocketClient/Classes/BaeWebSocketClient.cs
@@ -2,6 +2,7 @@
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Net.WebSockets;
 using System.Security.Cryptography;
@@ -10,105 +11,83 @@ using System.Threading.Tasks;
 
 namespace WebSocketClient.Classes
 {
-	public static class BaeWebSocketClient
-	{
-		private static ClientWebSocket _client = new();
-		private static readonly Uri _serverUri = new("ws://BaeIptimeDDNSAddress.iptime.org:49693/bae");
+    public static class BaeWebSocketClient
+    {
+        private static ClientWebSocket _client = new();
+        private static readonly Uri _serverUri = new("ws://BaeIptimeDDNSAddress.iptime.org:49693/bae");
 
 
-		public static async Task<Tuple<bool, string, JObject>> Connect(string id, string pw)
-		{
-			try
-			{
+        public static async Task<JObject> Connect(string id, string pw)
+        {
+            try
+            {
                 _client = new();
 
-                var task_conn = _client.ConnectAsync(_serverUri, CancellationToken.None);
-				if (task_conn.Wait(1000) == false)
-					throw new Exception("Fail to connet");
+                await _client.ConnectAsync(_serverUri, CancellationToken.None);
 
-                Console.WriteLine("Connected to WebSocket server.");
+                string crypt_pw = BitConverter.ToString(SHA256.Create().ComputeHash(Encoding.UTF8.GetBytes(pw))).Replace("-", "").ToLower();
 
-				string crypt_pw = "";
-				using (SHA256 sha256Hash = SHA256.Create())
-				{
-					byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(pw));
-					var builder = new StringBuilder();
-					for (int i = 0; i < bytes.Length; i++)
-						builder.Append(bytes[i].ToString("x2"));
+                var data = new JObject
+                {
+                    { "id", id },
+                    { "pw", crypt_pw },
+                };
 
-					crypt_pw = builder.ToString();
-				}
-
-				var data = new JObject
-				{
-					{ "id", id },
-					{ "pw", crypt_pw },
-				};
-
-				var task_login = RequestService("auth", "login", data);
-                if (task_conn.Wait(1000) == false)
-                    throw new Exception("Fail to login");
-
-                return new Tuple<bool, string, JObject>
-                (
-                    true,
-                    "",
-                    task_login.Result
-                );
+                return await RequestService("auth", "login", data);
             }
-			catch(Exception ex) {
-				return new Tuple<bool, string, JObject>
-				(
-					false,
-					ex.Message,
-					new JObject
-					{
-						{ "type", "rep" },
-						{ "result", 401 },
-						{ "msg",  ex.Message },
-						{ "data", new JArray() },
-					}
-				);
-			}
-		}
-		public static async Task Close()
-		{
-			await _client.CloseAsync(WebSocketCloseStatus.NormalClosure, "Client closed connection", CancellationToken.None);
-			Console.WriteLine("WebSocket connection closed.");
-		}
+            catch (Exception ex)
+            {
+                return new JObject
+                    {
+                        { "type", "rep" },
+                        { "result", 401 },
+                        { "msg",  ex.Message },
+                        { "data", new JArray() },
+                    };
+            }
+        }
+        public static async Task Close()
+        {
+            try
+            {
+                await _client.CloseAsync(WebSocketCloseStatus.NormalClosure, "Client closed connection", CancellationToken.None);
+                Debug.WriteLine("WebSocket connection closed.");
+            }
+            catch { }
+        }
 
-		public static async Task<JObject> RequestService(string service_name, string service_type, JObject json_data)
-		{
-			try
-			{
-				var jsonObject = new JObject
-				{
-					{ "service", service_name},
-					{ "type", service_type },
-					{ "data", json_data },
-				};
-				var encodedMessage = Encoding.UTF8.GetBytes(jsonObject.ToString());
+        public static async Task<JObject> RequestService(string service_name, string service_type, JObject json_data)
+        {
+            try
+            {
+                var jsonObject = new JObject
+                {
+                    { "service", service_name},
+                    { "type", service_type },
+                    { "data", json_data }
+                };
+                var encodedMessage = Encoding.UTF8.GetBytes(jsonObject.ToString());
 
 
-				var send_buffer = new ArraySegment<byte>(encodedMessage, 0, encodedMessage.Length);
-				await _client.SendAsync(send_buffer, WebSocketMessageType.Text, true, CancellationToken.None);
-
+                var send_buffer = new ArraySegment<byte>(encodedMessage, 0, encodedMessage.Length);
+                await _client.SendAsync(send_buffer, WebSocketMessageType.Text, true, CancellationToken.None);
 
                 var recv_buffer = new byte[1024];
                 var result = await _client.ReceiveAsync(new ArraySegment<byte>(recv_buffer), CancellationToken.None);
                 var receivedMessage = Encoding.UTF8.GetString(recv_buffer, 0, result.Count);
 
-				return JObject.Parse(receivedMessage);
-			}
-			catch(Exception ex) {
-				return new JObject
-					{
-						{ "type", "rep" },
-						{ "result", 401 },
-						{ "msg", ex.Message },
-						{ "data", new JArray() },
-					};
-			}
-		}
-	}
+                return JObject.Parse(receivedMessage);
+            }
+            catch (Exception ex)
+            {
+                return new JObject
+                    {
+                        { "type", "rep" },
+                        { "result", 401 },
+                        { "msg", ex.Message },
+                        { "data", new JArray() },
+                    };
+            }
+        }
+    }
 }
--- WebSocketClient/Pages/MainPage.xaml.cs
+++ WebSocketClient/Pages/MainPage.xaml.cs
@@ -1,35 +1,70 @@
 using System.Net.WebSockets;
+using System.Diagnostics;
 using System.Security.Cryptography;
 using System.Text;
 using WebSocketClient.Classes;
+using Newtonsoft.Json.Linq;
 
 namespace WebSocketClient.Pages;
 
 
 public partial class MainPage : ContentPage
 {
-    private ClientWebSocket _ws = null;
+	private ClientWebSocket _ws = null;
 
 	public MainPage()
 	{
 		InitializeComponent();
 	}
 
-    private void OnTestClicked(object sender, EventArgs e)
-    {
-        string user_id = Preferences.Get("user_id", "");
-        string user_pw = Preferences.Get("user_pw", "");
+	private async void OnTestClicked(object sender, EventArgs e)
+	{
+		string user_id = Preferences.Get("user_id", "");
+		string user_pw = Preferences.Get("user_pw", "");
+
+        TestStr.Text = "";
+
+        var delayTask = Task.Delay(1000);
 
         var task_conn = BaeWebSocketClient.Connect(user_id, user_pw);
-        if (task_conn.Wait(1000))
-        {
-            var ret = task_conn.Result;
-
-            Console.WriteLine(ret.ToString());
-        }
-        else
+        var completedTask = await Task.WhenAny(task_conn, delayTask);
+        if (completedTask == delayTask)
         {
             BaeWebSocketClient.Close().Wait();
+            TestStr.Text = "Fail to auth";
+            return;
         }
+
+		var ret = await task_conn;
+        if (ret["result"].Value<int>() != 200)
+        {
+            TestStr.Text = "Fail to auth";
+            return;
+        }
+
+        var data = new JObject
+        {
+            { "device_name", "Bae-DeskTop" },
+        };
+
+        delayTask = Task.Delay(1000);
+        var type_list = BaeWebSocketClient.RequestService("wol", "wol_device", data);
+        completedTask = await Task.WhenAny(type_list, delayTask);
+        if (completedTask == delayTask)
+        {
+            BaeWebSocketClient.Close().Wait();
+            TestStr.Text = "Fail to get list";
+            return;
+        }
+
+        ret = await type_list;
+        if (ret["result"].Value<int>() != 200)
+        {
+            TestStr.Text = "Fail to get list";
+            return;
+        }
+
+
+        TestStr.Text = ret.ToString();
     }
 }
